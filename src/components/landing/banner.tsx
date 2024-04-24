6a97e37051b7db7b73174c48d6eb91690bce4db0
ScottDeev <amehelijah99@gmai.com>
2024-04-24 08:08:38 +0100

    Banner section update --in progress

---
+++ src/components/landing/banner.tsx
@@ -0,0 +1,15 @@
+export default function Banner() {
+  return (
+    <section className="banner-section h-screen bg-[#000] flex justify-center p-[50px]">
+      <div>
+        <h1 className="text-[50px] text-[#fff] uppercase font-bold">
+          Rocket for Exploration
+        </h1>
+        <p className="text-[#fff]">
+          Embark on a Journey of Discovery and Innovation to Reach New Frontiers
+          of Space Exploration
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/landing/landingPage.tsx
@@ -0,0 +1,9 @@
+import Banner from "../../components/landing/banner";
+
+export default function LandingPage() {
+  return (
+    <main>
+      <Banner />
+    </main>
+  );
+}
